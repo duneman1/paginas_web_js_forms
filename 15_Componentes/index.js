1f736093f7a3d67ab6adbdfa873c886955562c26
duneman1 <duneman.basura@gmail.com>
2018-09-21 14:04:01 +0200

    controller

---
+++ 15_Componentes/index.js
@@ -0,0 +1,17 @@
+
+import { PageController } from './page.controller.js';
+
+
+class Index extends PageController{
+    constructor() {
+        super()
+
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => { new Index()})
+
+
+
+
---
+++ 15_Componentes/pages/about.js
@@ -0,0 +1,17 @@
+import { PageController } from "../page.controller.js";
+
+
+
+class About extends PageController {
+    constructor() {
+        super('About', 'Acerca de')
+
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => { new About()})
+
+
+
+
