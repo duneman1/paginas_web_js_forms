b1da3cd65bdd23363f2c0d54bc9f5d284f18897e
duneman1 <duneman.basura@gmail.com>
2018-09-25 12:55:04 +0200

    Type script y angular

---
+++ angular18/ts/sample.ts
@@ -0,0 +1,17 @@
+function main() {
+    let x : number = 23
+    let name: string = ''
+    let isCasado : boolean
+    let anyValue : any
+    let oDato : Object
+    let aDatos : Array<any>
+    //let aDatos2 : []
+    console.log(x)  
+    name = 'José María'
+    console.log(name)
+    isCasado = true
+    oDato = {}
+    aDatos = [12,12]
+}
+
+main()
