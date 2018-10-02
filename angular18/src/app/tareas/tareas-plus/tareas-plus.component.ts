618669380b6f57062c13b02b9e43e4934bbd87a6
duneman1 <duneman.basura@gmail.com>
2018-10-02 13:46:34 +0200

    tareas plus angular

--- angular18/src/app/tareas/tareas-plus/tareas-plus.component.ts
+++ angular18/src/app/tareas/tareas-plus/tareas-plus.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TareaModel, TareaIf } from '../../models/tarea.model';
+
+
+
 
 @Component({
   selector: 'cas-tareas-plus',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TareasPlusComponent implements OnInit {
 
+  titulo: string
+  rutaGoogle: string
+  //newTarea: TareaModel
+  newTarea: TareaIf //POJO
+  aTareas: Array<TareaIf>
+
   constructor() { }
 
   ngOnInit() {
+    this.titulo = 'Lista Plus'
+    this.rutaGoogle= 'https://www.google.es'
+    //this.newTarea = new TareaModel('', false)
+    this.newTarea = {nombre: '', isCompleted: false}
+    this.aTareas = []
+  }
+  
+  addTarea () {
+
+    if(!this.newTarea.nombre) {return}
+    //let tareaClon = JSON.parse(JSON.stringify(this.newTarea))
+
+    let tareaClon = Object.assign({}, this.newTarea)
+    this.aTareas.push(tareaClon)
+    //this.newTarea = new TareaModel(this.tareaName, false)
+    console.log (this.aTareas)
+    this.newTarea.nombre = ''
+
   }
 
+  
+  borrarTarea(i) {
+    this.aTareas.splice(i,1)
+  }
 }
